0da2080f10e584baf04f4cfd82a98f3fb48c89bb
georgekaranja <167513991+geoegekaranja@users.noreply.github.com>
2026-03-12 16:02:16 +0300

    r

--- includes/header.php
+++ includes/header.php
@@ -1,34 +1,69 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
 
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 <title>Mend Nest KE</title>
+
+<!-- Bootstrap CSS -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-<link rel="stylesheet" href="/mendnest/css/style.css">
+
+<!-- Custom CSS -->
+<link rel="stylesheet" href="css/style.css">
 
 </head>
 
 <body>
 
-<nav class="navbar navbar-expand-lg bg-light">
+<nav class="navbar navbar-expand-lg navbar-light bg-light shadow-sm">
 
 <div class="container">
 
-<a class="navbar-brand" href="index.php">
+<a class="navbar-brand fw-bold" href="index.php">
 Mend Nest KE
 </a>
 
+<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+<span class="navbar-toggler-icon"></span>
+</button>
+
+<div class="collapse navbar-collapse" id="navbarNav">
+
 <ul class="navbar-nav ms-auto">
 
-<li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
-<li class="nav-item"><a class="nav-link" href="about.php">About</a></li>
-<li class="nav-item"><a class="nav-link" href="services.php">Services</a></li>
-<li class="nav-item"><a class="nav-link" href="consultation.php">Consultation</a></li>
-<li class="nav-item"><a class="nav-link" href="blog.php">Blog</a></li>
-<li class="nav-item"><a class="nav-link" href="testimonials.php">Testimonials</a></li>
-<li class="nav-item"><a class="nav-link" href="contact.php">Contact</a></li>
+<li class="nav-item">
+<a class="nav-link" href="index.php">Home</a>
+</li>
+
+<li class="nav-item">
+<a class="nav-link" href="about.php">About</a>
+</li>
+
+<li class="nav-item">
+<a class="nav-link" href="services.php">Services</a>
+</li>
+
+<li class="nav-item">
+<a class="nav-link" href="consultation.php">Consultation</a>
+</li>
+
+<li class="nav-item">
+<a class="nav-link" href="blog.php">Blog</a>
+</li>
+
+<li class="nav-item">
+<a class="nav-link" href="testimonials.php">Testimonials</a>
+</li>
+
+<li class="nav-item">
+<a class="nav-link" href="contact.php">Contact</a>
+</li>
 
 </ul>
+
+</div>
 
 </div>
 
