910a36a791a5bc9622237d311bc81327b439682e
georgekaranja <167513991+geoegekaranja@users.noreply.github.com>
2026-08-13 22:08:05 +0300

    r

--- includes/footer.php
+++ includes/footer.php
@@ -1,11 +1,12 @@
 <footer class="bg-dark text-white text-center p-4 mt-5">
 
-<p>© <?php echo date("Y"); ?> Mend Nest KE Home Nursing</p>
+    <p>© <?php echo date("Y"); ?> Mend Nest KE Home Nursing</p>
 
-<p>Spur Mall, 2nd Floor S32 | Nairobi Kenya</p>
+    <p>Spur Mall, 2nd Floor S32 | Nairobi Kenya</p>
 
 </footer>
 
+<!-- AOS JavaScript -->
 <script src="https://unpkg.com/aos@2.3.4/dist/aos.js"></script>
 
 <script>
@@ -13,9 +14,10 @@ AOS.init({
     duration: 1000,
     once: true
 });
+</script>
+
+<!-- Bootstrap JavaScript -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
 
 </body>
 </html>
--- includes/header.php
+++ includes/header.php
@@ -28,28 +28,56 @@
 
 <!-- NAVBAR -->
 <nav class="navbar navbar-expand-lg navbar-light bg-light shadow-sm">
-  <div class="container">
-    <a class="navbar-brand fw-bold" href="#">Mend Nest KE</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav ms-auto">
-        <li class="nav-item">
-          <a class="nav-link active" href="/mendnest/index.php">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/mendnest/pages/consultation.php">Consultation</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/mendnest/pages/services.php">Services</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/mendnest/pages/contact.php">Contact</a>
-          <li class="nav-item">
-          <a class="nav-link" href="/mendnest/pages/blog.php">Blog</
-        </li>
-      </ul>
+    <div class="container">
+
+        <a class="navbar-brand fw-bold" href="/mendnest/index.php">
+            Mend Nest KE
+        </a>
+
+        <button class="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarNav"
+                aria-controls="navbarNav"
+                aria-expanded="false"
+                aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+
+                <li class="nav-item">
+                    <a class="nav-link active" href="/mendnest/index.php">
+                        Home
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="/mendnest/pages/consultation.php">
+                        Consultation
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="/mendnest/pages/services.php">
+                        Services
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="/mendnest/pages/contact.php">
+                        Contact
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="/mendnest/pages/blog.php">
+                        Blog
+                    </a>
+                </li>
+
+            </ul>
+        </div>
     </div>
-  </div>
 </nav>
